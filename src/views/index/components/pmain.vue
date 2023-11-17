15a33897005183d18bcbfe1c537b5b4178da4101
bronya98 <54210871+yang97167880@users.noreply.github.com>
2023-11-18 00:06:53 +0800

    update swiper (#11)

--- src/views/index/components/pmain.vue
+++ src/views/index/components/pmain.vue
@@ -1,11 +1,11 @@
 <script>
 import Swiper from "@/views/index/components/swiper.vue";
-import {partitions, recommendPartition, recommends} from "@/api/recommend";
+import { partitions, recommendPartition, recommends } from "@/api/recommend";
 import PreviewPlayer from "@/components/preview-player.vue";
 import PreviewVideoCard from "@/components/preview-video-card.vue";
 
 export default {
-  components: {PreviewVideoCard, VideoCard: PreviewVideoCard, PreviewPlayer, Swiper},
+  components: { PreviewVideoCard, VideoCard: PreviewVideoCard, PreviewPlayer, Swiper },
   data() {
     return {
       feed: {
@@ -41,7 +41,7 @@ export default {
 
     },
     scrollTop() {
-      window.scrollTo({top: 0, behavior: 'smooth'})
+      window.scrollTo({ top: 0, behavior: 'smooth' })
     },
     request_recommends() {
       this.scrollTop()
@@ -53,9 +53,9 @@ export default {
       this.requested_partition = []
       this.partition_feed = []
       recommends(6, 6, 5)
-          .then(res => {
-            this.feed = res.data
-          })
+        .then(res => {
+          this.feed = res.data
+        })
       this.request_partition_recommend()
 
     },
@@ -82,19 +82,19 @@ export default {
           }
           that.partitions_requesting = true
           recommendPartition(that.partitions[0], "投稿时间", 1, 5)
-              .then(res => {
-                this.partitions_requesting = false
-                if (res.data.total > 0) {
-                  this.partition_feed.push({
-                    partition: that.partitions[0],
-                    value: res.data.page
-                  })
-                  load_count++
-                }
-                this.requested_partition.push(that.partitions.shift())
-              }).catch(err => {
-            that.partitions_requesting = false
-          })
+            .then(res => {
+              this.partitions_requesting = false
+              if (res.data.total > 0) {
+                this.partition_feed.push({
+                  partition: that.partitions[0],
+                  value: res.data.page
+                })
+                load_count++
+              }
+              this.requested_partition.push(that.partitions.shift())
+            }).catch(err => {
+              that.partitions_requesting = false
+            })
         } else {
           clearInterval(loop)
         }
@@ -121,29 +121,29 @@ export default {
     <div class="first-feed-panel">
       <!--   轮播   -->
       <div style="grid-row: span 2;grid-column: span 2;">
-        <swiper :swiper="feed.swiper"/>
+        <swiper :swiper="feed.swiper" />
       </div>
-      <preview-video-card v-for="feed in feed.first" :bvod="feed" :id_suffix="'first_feed'"/>
+      <preview-video-card v-for="feed in feed.first" :bvod="feed" :id_suffix="'first_feed'" />
 
     </div>
     <!--  分类推荐  -->
-    <div class="first-feed-panel" :class="{'empty-feed-panel':type}" v-for="type in partition_feed">
-      <preview-video-card style="width: 100%;height: 100%" v-for="(bvod,idx) in type.value" :bvod="bvod"
-                          :id_suffix="bvod.partition" :show_partition="idx===type.value.length-1"/>
+    <div class="first-feed-panel" :class="{ 'empty-feed-panel': type }" v-for="type in partition_feed">
+      <preview-video-card style="width: 100%;height: 100%" v-for="(bvod, idx) in type.value" :bvod="bvod"
+        :id_suffix="bvod.partition" :show_partition="idx === type.value.length - 1" />
     </div>
 
     <!--  右侧刷新按钮  -->
-    <div style="position: absolute;right: 0;width: 10%;padding-left: 1%;top: 50px">
+    <div style="position: absolute;right: 0;width: 10%;padding-left: 1%;margin-top: 64px;">
       <div class="refresh-btn" @click="request_recommends">
-        <div class="refresh-icon" :class="{'rotation-animation':refreshing}">
+        <div class="refresh-icon" :class="{ 'rotation-animation': refreshing }">
           <svg t="1697409301924" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-               p-id="8547" width="16" height="16">
+            p-id="8547" width="16" height="16">
             <path
-                d="M126.4 870.4c-17.6 0-32-14.4-32-32V665.6c0-17.6 14.4-32 32-32s32 14.4 32 32v172.8c0 17.6-14.4 32-32 32zM897.6 390.4c-17.6 0-32-14.4-32-32V185.6c0-17.6 14.4-32 32-32s32 14.4 32 32v172.8c0 17.6-14.4 32-32 32z"
-                fill="#bfbfbf" p-id="8548"></path>
+              d="M126.4 870.4c-17.6 0-32-14.4-32-32V665.6c0-17.6 14.4-32 32-32s32 14.4 32 32v172.8c0 17.6-14.4 32-32 32zM897.6 390.4c-17.6 0-32-14.4-32-32V185.6c0-17.6 14.4-32 32-32s32 14.4 32 32v172.8c0 17.6-14.4 32-32 32z"
+              fill="#bfbfbf" p-id="8548"></path>
             <path
-                d="M512 960c-184 0-347.2-110.4-416-281.6-6.4-16 1.6-35.2 17.6-41.6 16-6.4 35.2 1.6 41.6 17.6C214.4 801.6 353.6 896 512 896s297.6-94.4 356.8-241.6c6.4-16 25.6-24 41.6-17.6 16 6.4 24 25.6 17.6 41.6C859.2 849.6 696 960 512 960zM897.6 390.4c-12.8 0-24-8-30.4-20.8C809.6 222.4 670.4 128 512 128S214.4 222.4 155.2 369.6c-6.4 16-25.6 24-41.6 17.6-16-6.4-24-25.6-17.6-41.6C164.8 174.4 328 64 512 64s347.2 110.4 416 281.6c6.4 16-1.6 35.2-17.6 41.6-4.8 1.6-8 3.2-12.8 3.2z"
-                fill="#bfbfbf" p-id="8549"></path>
+              d="M512 960c-184 0-347.2-110.4-416-281.6-6.4-16 1.6-35.2 17.6-41.6 16-6.4 35.2 1.6 41.6 17.6C214.4 801.6 353.6 896 512 896s297.6-94.4 356.8-241.6c6.4-16 25.6-24 41.6-17.6 16 6.4 24 25.6 17.6 41.6C859.2 849.6 696 960 512 960zM897.6 390.4c-12.8 0-24-8-30.4-20.8C809.6 222.4 670.4 128 512 128S214.4 222.4 155.2 369.6c-6.4 16-25.6 24-41.6 17.6-16-6.4-24-25.6-17.6-41.6C164.8 174.4 328 64 512 64s347.2 110.4 416 281.6c6.4 16-1.6 35.2-17.6 41.6-4.8 1.6-8 3.2-12.8 3.2z"
+              fill="#bfbfbf" p-id="8549"></path>
           </svg>
         </div>
 
@@ -154,23 +154,23 @@ export default {
   </div>
   <!--  锚点  -->
   <div style="position:fixed;right: 5%;bottom: 20%;display: flex;justify-content: center;flex-direction: column"
-       v-show="show_anchor">
+    v-show="show_anchor">
     <div class="refresh-batch" @click="request_recommends">
-      <div class="refresh-icon" :class="{'rotation-animation':refreshing}">
+      <div class="refresh-icon" :class="{ 'rotation-animation': refreshing }">
         <svg t="1697409301924" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-             p-id="8547" width="22" height="22">
+          p-id="8547" width="22" height="22">
           <path
-              d="M126.4 870.4c-17.6 0-32-14.4-32-32V665.6c0-17.6 14.4-32 32-32s32 14.4 32 32v172.8c0 17.6-14.4 32-32 32zM897.6 390.4c-17.6 0-32-14.4-32-32V185.6c0-17.6 14.4-32 32-32s32 14.4 32 32v172.8c0 17.6-14.4 32-32 32z"
-              fill="#ffffff" p-id="8548"></path>
+            d="M126.4 870.4c-17.6 0-32-14.4-32-32V665.6c0-17.6 14.4-32 32-32s32 14.4 32 32v172.8c0 17.6-14.4 32-32 32zM897.6 390.4c-17.6 0-32-14.4-32-32V185.6c0-17.6 14.4-32 32-32s32 14.4 32 32v172.8c0 17.6-14.4 32-32 32z"
+            fill="#ffffff" p-id="8548"></path>
           <path
-              d="M512 960c-184 0-347.2-110.4-416-281.6-6.4-16 1.6-35.2 17.6-41.6 16-6.4 35.2 1.6 41.6 17.6C214.4 801.6 353.6 896 512 896s297.6-94.4 356.8-241.6c6.4-16 25.6-24 41.6-17.6 16 6.4 24 25.6 17.6 41.6C859.2 849.6 696 960 512 960zM897.6 390.4c-12.8 0-24-8-30.4-20.8C809.6 222.4 670.4 128 512 128S214.4 222.4 155.2 369.6c-6.4 16-25.6 24-41.6 17.6-16-6.4-24-25.6-17.6-41.6C164.8 174.4 328 64 512 64s347.2 110.4 416 281.6c6.4 16-1.6 35.2-17.6 41.6-4.8 1.6-8 3.2-12.8 3.2z"
-              fill="#ffffff" p-id="8549"></path>
+            d="M512 960c-184 0-347.2-110.4-416-281.6-6.4-16 1.6-35.2 17.6-41.6 16-6.4 35.2 1.6 41.6 17.6C214.4 801.6 353.6 896 512 896s297.6-94.4 356.8-241.6c6.4-16 25.6-24 41.6-17.6 16 6.4 24 25.6 17.6 41.6C859.2 849.6 696 960 512 960zM897.6 390.4c-12.8 0-24-8-30.4-20.8C809.6 222.4 670.4 128 512 128S214.4 222.4 155.2 369.6c-6.4 16-25.6 24-41.6 17.6-16-6.4-24-25.6-17.6-41.6C164.8 174.4 328 64 512 64s347.2 110.4 416 281.6c6.4 16-1.6 35.2-17.6 41.6-4.8 1.6-8 3.2-12.8 3.2z"
+            fill="#ffffff" p-id="8549"></path>
         </svg>
       </div>
 
     </div>
     <div class="anchor" @click="scrollTop">
-      <Icon type="md-arrow-dropup" size="22"/>
+      <Icon type="md-arrow-dropup" size="22" />
       <span>顶部</span>
     </div>
   </div>
@@ -183,6 +183,7 @@ export default {
   position: relative;
   display: flex;
   flex-direction: column;
+  align-items: flex-start;
   padding-top: 1%;
   padding-left: 10%;
   padding-right: 10%;
@@ -195,7 +196,7 @@ export default {
   gap: 30px 10px;
   grid-gap: 30px 10px;
   width: 100%;
-  margin-top: 3%;
+  margin-top: 64px;
 }
 
 .empty-feed-panel {
@@ -206,6 +207,7 @@ export default {
   0% {
     background-position: 0 0;
   }
+
   100% {
     background-position: 100% 100%;
   }
@@ -213,7 +215,6 @@ export default {
 
 
 .refresh-btn {
-  margin-top: 5px;
   writing-mode: vertical-lr;
   text-orientation: mixed;
   border: #f1f1f1 1px solid;
@@ -268,6 +269,7 @@ export default {
   from {
     transform: rotate(0deg);
   }
+
   to {
     transform: rotate(270deg);
   }
@@ -284,5 +286,4 @@ export default {
 .rotation-animation {
   animation: rotate_720 .5s ease-in-out;
 }
-
 </style>
