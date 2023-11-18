427c80b1116bf59c89a16a9b4591516c8aa5721d
jerry <140386566+CxyJerry@users.noreply.github.com>
2023-11-18 22:59:38 +0800

    feat: support email login (#17)

--- src/api/user.js
+++ src/api/user.js
@@ -18,11 +18,34 @@ function login(tel, code) {
     })
 }
 
+function email_login(email, code) {
+    return Api.post('/user/email-login', {
+        email: email,
+        verifyCode: code
+    }).then(res => {
+        Message.success({
+            content: '登录成功'
+        })
+        store.commit('set_user_info', res.data)
+        store.commit('set_has_login', true)
+        store.commit('set_login_modal_visible', false)
+        idles()
+    })
+}
+
 
 function code(tel) {
     return Api.get('/user/code', {
         params: {
             tel: tel
+        }
+    })
+}
+
+function email_code(email) {
+    return Api.get('/user/email-code', {
+        params: {
+            email: email
         }
     })
 }
@@ -56,7 +79,9 @@ function user_page(page_no, page_size) {
 
 export {
     login,
+    email_login,
     code,
+    email_code,
     logout,
     user_info,
     user_page
